3edcf1a9773f6ea8760c2840ecdf0aeabc39990c
Jinzhou Zhang <lotabout@gmail.com>
2021-01-09 19:06:02 +0800

    support additional jar files for custom ops

--- src/main/java/me/lotabout/ConsoleCalculator.java
+++ src/main/java/me/lotabout/ConsoleCalculator.java
@@ -2,6 +2,7 @@ package me.lotabout;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
@@ -23,6 +24,18 @@ public class ConsoleCalculator {
       "Type in RRNs(Reverse Polish Notation) to start calculation, Ctrl-D to exit\n";
 
   public static void main(String[] args) {
+    if (args.length > 0) {
+      OperatorLoader loader = new OperatorLoader();
+      for (String fileOrDirectory : args) {
+        try {
+          loader.addJarOrDir(fileOrDirectory);
+        } catch (MalformedURLException ex) {
+          log.info("Failed to add jar file to loader: {}", fileOrDirectory, ex);
+        }
+      }
+      Thread.currentThread().setContextClassLoader(loader);
+    }
+
     List<OperatorReader<RealNumber>> readers = loadReaderImplementations();
     Tokenizer<RealNumber> tokenizer = new RegexTokenizer<>(readers);
     Printer<RealNumber> printer = new SimplePrinter<>();
